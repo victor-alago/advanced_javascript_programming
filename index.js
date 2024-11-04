300902495288b82b2159d99fcbf8827e5d5eefdc
Alago Victor <123035877+victor-alago@users.noreply.github.com>
2024-11-04 15:26:20 +0100

    Finished up first part of the exercises

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 console.log('Testing node app')
+
+// Part 0
+// Q1
+const student = {
+    name: 'Marie',
+    age: 25,
+    courses: []
+}
+
+
+// Q2
+student.age = 21
+
+student.grade = 'A'
+// console.log(student)
+
+
+// Q3
+student.courses.push('Math', 'Physics', 'Chemistry')
+
+const courseIndex = student.courses.indexOf('Physics')
+// console.log(courseIndex)
+
+const newCourses = student.courses.slice(0, 2)
+// console.log(newCourses)
+
+
+// Q4
+console.log(student)
+console.log(courseIndex)
+console.log(newCourses)
+
+
+
+// Part 1
